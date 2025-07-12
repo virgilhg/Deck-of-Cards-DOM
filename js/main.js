39d839df003a1f5e6b6b1101cb39122d60d786e0
Fergster1299 <fergdyl@gmail.com>
2025-07-12 19:16:56 -0400

    adding player 2 js

--- js/main.js
+++ js/main.js
@@ -53,6 +53,41 @@ function cardValueConverter(val){
 }
 
 
+// Dylan's Code
+
+
+let deckId2 = ''
+
+fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+.then(res => res.json()) // parse response as JSON
+.then(data => {
+  console.log(data)
+  deckId2 = data.deck_id
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
+
+
+document.querySelector('button').addEventListener('click', getPlayer2)
+
+function getPlayer2(){
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+
+  fetch(url)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+    document.querySelector('#player2').src = data.cards[0].image
+    let player2Value = cardValueConverter(data.cards[0].value)
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
+}
+
+
+
 
 
 
