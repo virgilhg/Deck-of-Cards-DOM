d1a3ea089402a820bc57561abc3c4ffd29bd258b
Fergster1299 <fergdyl@gmail.com>
2025-07-12 20:59:16 -0400

    merged functions together and deleted needless code

--- js/main.js
+++ js/main.js
@@ -1,41 +1,46 @@
-//Get the deck
+document.querySelector('button').addEventListener('click', compare)
+
 let deckId = ""
 
 fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data)
       deckId = data.deck_id
        
     })
     .catch(err => {
       console.log(`error ${err}`)
     });
-document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
-  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
-  fetch(url)
-    .then (res => res.json())
-    .then (data => {
-      console.log(data)
-      const player1Val= Number(cardValueConverter(data.cards[0].value))
-      document.querySelector('#player1').src = data.cards[0].image
-    })
-    /* if(player1.val< player2.val){
-          document.querySelector("h3").innerText= "Player 2 won"
-       }
-       else if(player1.val> player2.val){
-          document.querySelector("h3").innerText= "Player 1 won"
-       }
-       else{
-          document.querySelector("h3").innerText= "It's time for War"
-       }
-    */
-      .catch(err =>{
-      console.log(`error ${err}`)
-    })
+function compare(){
+const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+
+  let player1 = getFetch(url)
+
+  function getFetch(val){
+    fetch(val)
+      .then (res => res.json())
+      .then (data => {
+        document.querySelector('#player1').src = data.cards[0].image
+        let player1 = cardValueConverter(data.cards[0].value)
+
+        document.querySelector('#player2').src = data.cards[1].image
+        let player2 = cardValueConverter(data.cards[1].value)
+
+        if(player1 > player2){
+          document.querySelector('#compare').innerHTML = 'Player 1 Wins!'
+        }else if(player1 < player2){
+          document.querySelector('#compare').innerHTML = 'Player 2 Wins!'
+        }else{
+          document.querySelector('#compare').innerHTML = "It's a tie!!"
+        }
+      })
+        .catch(err =>{
+        console.log(`error ${err}`)
+      })
+  }
 }
+
 
 function cardValueConverter(val){
   if(val === "KING"){
@@ -49,47 +54,7 @@ function cardValueConverter(val){
     {return 11}
   else
     return Number(val)
-
 }
-
-
-// Dylan's Code
-
-
-let deckId2 = ''
-
-fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-.then(res => res.json()) // parse response as JSON
-.then(data => {
-  console.log(data)
-  deckId2 = data.deck_id
-})
-.catch(err => {
-    console.log(`error ${err}`)
-});
-
-
-document.querySelector('button').addEventListener('click', getPlayer2)
-
-function getPlayer2(){
-  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-
-  fetch(url)
-  .then(res => res.json()) // parse response as JSON
-  .then(data => {
-    console.log(data)
-    document.querySelector('#player2').src = data.cards[0].image
-    let player2Value = cardValueConverter(data.cards[0].value)
-  })
-  .catch(err => {
-      console.log(`error ${err}`)
-  });
-}
-
-
-
-
-
 
 
 
